b14a2222ec52f07c458d459a9768dafa7dd774fa
tktk <tktk.stereoman@gmail.com>
2017-02-05 07:38:00 +0900

    Update step.py

--- step.py
+++ step.py
@@ -17,20 +17,17 @@ THRESHOLD = 500
 def count():
         count = 0
         left_flag = False
+	prev_value = 0
 	while MAX_COUNT >= count:
 		res_left = spi_left.xfer2([0x68, 0x00])
 		value_left = (res_left[0] * 256 + res_left[1]) & 0x3ff
 		res_right = spi_right.xfer2([0x68, 0x00])
 		value_right = (res_right[0] * 256 + res_right[1]) & 0x3ff
-                
+
 		# print "left: " + str(value_left)
-		if left_flag is False and value_left > value_right and value_left > THRESHOLD:
-			left_flag = True
-			count += 1
-		        print count
-		elif left_flag is True and value_left < value_right and value_right > THRESHOLD:
-			left_flag = False
+		if value_left - prev_value > THRESHOLD:
 			count += 1
 		        print count
 		# print "right: " + str(value_right)
+		prev_value = value_left
 		time.sleep(0.1)   #sleep
