4c5635546abf7e47457c7bb11e2186301569b2f7
tk3fftk <tktk.stereoman@gmail.com>
2017-02-04 12:22:15 +0000

    only left is working

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import spidev
 import time
 import subprocess
 
 # open spi devices
-spi = spidev.SpiDev()
-spi.open(0, 0)
+spi_left = spidev.SpiDev()
+spi_right= spidev.SpiDev()
+
+spi_left.open(0, 0)
+spi_right.open(0, 1)
+
+count = 0
 
 try:
     while True:
-        res = spi.xfer2([0x68, 0x00])
-        value = (res[0] * 256 + res[1]) & 0x3ff
-        print value
-        time.sleep(1)   #sleep 1sec
+        res_left = spi_left.xfer2([0x68, 0x00])
+        value_left = (res_left[0] * 256 + res_left[1]) & 0x3ff
+        print "left: " + str(value_left)
+	# if value_left > 500:
+#		count += 1
+	# print count	
+        res_right = spi_right.xfer2([0x68, 0x00])
+        value_right = (res_right[0] * 256 + res_right[1]) & 0x3ff
+        print "right: " + str(value_right)
+        time.sleep(0.1)   #sleep 1sec
 except KeyboardInterrupt:
-    spi.close()
+    spi_left.close()
+    spi_right.close()
